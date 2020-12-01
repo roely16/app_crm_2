a08bedf29786bb973699d3985b2f2e1702dee228
Roely Chur <gerson.roely@gmail.com>
2020-12-01 15:56:40 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ?
         '/apps/app_crm/' : '/',
     devServer: {
-        host: 'localhost'
+        host: 'localhost' 
     }
 }
